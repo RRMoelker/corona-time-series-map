12ff1b330bfeae278c227474b7a151b12bfe1c3c
RM <RRMoelker@users.noreply.github.com>
2020-02-25 11:09:30 +0100

    boilerplate leaflet map included

--- src/App.svelte
+++ src/App.svelte
@@ -1,25 +1,31 @@
 <script>
+  import Map from './Map.svelte';
+
 	export let name;
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<p>Goals:</p>
+  <ul>
+    <li>Show virus progress over time</li>
+    <li>Visualize infection rate</li>
+    <li>Communicate both size, mortality while staying localized</li>
+  </ul>
+
+	<Map />
 </main>
 
 <style>
+  :global(html, body) {
+    height: 100%;
+    margin: 0;
+  }
+
 	main {
 		text-align: center;
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
 	}
 
 	@media (min-width: 640px) {
---
+++ src/Map.svelte
@@ -0,0 +1,26 @@
+<script>
+	import { onMount } from 'svelte';
+  import L from 'leaflet';
+	export let name;
+
+	onMount(async () => {
+    const LMap = L.map('map').setView([0, 0], 3);
+
+    const Wikimedia = L.tileLayer('https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}{r}.png', {
+    	attribution: '<a href="https://wikimediafoundation.org/wiki/Maps_Terms_of_Use">Wikimedia</a>',
+    	minZoom: 1,
+    	maxZoom: 19
+    });
+
+    Wikimedia.addTo(LMap);
+  });
+</script>
+
+<style>
+  #map {
+    width: 100%;
+    height: 400px;
+  }
+</style>
+
+<div id="map"></div>
