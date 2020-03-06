dc801f20a4f44b083445c34b0154993918992a61
RM <RRMoelker@users.noreply.github.com>
2020-03-06 20:51:05 +0100

    Route changes logged to analytics

---
+++ src/route-listener.js
@@ -0,0 +1,33 @@
+const tryLogRoute = (url) => {
+  window.dataLayer = window.dataLayer || [];
+  window.dataLayer.push({
+    'event': 'Pageview',
+    'pagePath': url,
+    'pageTitle': document.title
+  });
+};
+
+export const logRouteChanges = () => {
+  window.addEventListener('locationchange', function(){
+    tryLogRoute(window.location.href);
+  });
+
+  // Listener logic based on: https://stackoverflow.com/a/52809105
+  history.pushState = ( f => function pushState(){
+    var ret = f.apply(this, arguments);
+    window.dispatchEvent(new Event('pushstate'));
+    window.dispatchEvent(new Event('locationchange'));
+    return ret;
+  })(history.pushState);
+
+  history.replaceState = ( f => function replaceState(){
+    var ret = f.apply(this, arguments);
+    window.dispatchEvent(new Event('replacestate'));
+    window.dispatchEvent(new Event('locationchange'));
+    return ret;
+  })(history.replaceState);
+
+  window.addEventListener('popstate',()=>{
+    window.dispatchEvent(new Event('locationchange'))
+  });
+};
