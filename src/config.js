b0fd91cc96dc2d956e81b76a6501b4956499cc72
RM <RRMoelker@users.noreply.github.com>
2020-04-25 11:53:14 +0200

    Switch data on host

--- src/config.js
+++ src/config.js
@@ -2,4 +2,13 @@
 // Fork is kept up to date with main repo and also performs some coarse checks if the data is the same structure.
 // This is to prevent the app to stop working from a data update outside this apps control.
 // Serving through Netlify for CDN benefits
-export const DATA_URL = '/data/time_series_19-covid-Confirmed.csv';
+
+const fileName = 'time_series_19-covid-Confirmed.csv';
+let dataUrl;
+if (location && location.hostname === "localhost" || location && location.hostname === "127.0.0.1") {
+  dataUrl = `https://johns-hopkins-covid19-dataset-educational-fork.netlify.app/${fileName}`;
+} else {
+  dataUrl = `/data/${fileName}`;
+}
+
+export const DATA_URL = dataUrl;
