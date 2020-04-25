44e24c5aba5d643943b84488ecd3d8dafac7dc12
RM <RRMoelker@users.noreply.github.com>
2020-04-25 11:30:57 +0200

    using redirect

--- src/config.js
+++ src/config.js
@@ -2,4 +2,4 @@
 // Fork is kept up to date with main repo and also performs some coarse checks if the data is the same structure.
 // This is to prevent the app to stop working from a data update outside this apps control.
 // Serving through Netlify for CDN benefits
-export const DATA_URL = 'https://johns-hopkins-covid19-dataset-educational-fork.netlify.com/time_series_19-covid-Confirmed.csv';
+export const DATA_URL = '/data/time_series_19-covid-Confirmed.csv';
